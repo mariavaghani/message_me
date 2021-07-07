abd8adfd56e940ce5ddb7e9700882343711daa81
mariavaghani <maria.vaghani@gmail.com>
2021-07-07 16:53:44 -0400

    config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,13 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 module MessageMe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.assets.initialize_on_precompile = false
+    
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
